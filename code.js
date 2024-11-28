367b44f8bcd6ef3da84a320883fffd4e727fc19a
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-27 23:01:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 are_isomorphic(graph1, graph2) {
-    return undefined;
+
+    if(graph1.length !== graph2.length){
+        return false;
+    }
+
+    if(countEdges(graph1) !== countEdges(graph2)){
+        return false;
+    }
+    
+    let map = [];
+    let visited = []
+
+    //simular to the logic in graph-representations for edge traversal
+    function countEdges(graph){
+        let totalEdge =0;
+
+        for(let i=0;i < graph.length; i++){
+            let rowEdge = 0;
+
+            for (let j=0; j < graph[i].length; j++){
+                rowEdge += graph[i][j];
+            }
+            totalEdge += rowEdge;
+        }
+
+        return totalEdge;
+    }
 }
