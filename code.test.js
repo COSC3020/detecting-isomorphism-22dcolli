0ba68a1f5c3bbb65f94e4668b4c27e9b5b44a009
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-28 01:41:08 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -15,4 +15,20 @@ let graph2 = [
     [0, 1, 0]
 ];
 
+let fullyConnected1 =[
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+];
+
+let fullyConnected2 =[
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+];
+    
+
 assert(are_isomorphic(graph1, graph2) === true);
+assert(are_isomorphic(fullyConnected1, fullyConnected2) === true);
