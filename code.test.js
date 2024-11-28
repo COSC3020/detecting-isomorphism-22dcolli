299c239461a3fd25844a5e6a238b8d2d2c979840
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-28 00:31:49 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const assert = require('assert');
 
+eval(fs.readFileSync('code.js')+'');
+
+let graph1 = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+];
+
+let graph2 = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+];
+
+assert(are_isomorphic(graph1, graph2) === true;
