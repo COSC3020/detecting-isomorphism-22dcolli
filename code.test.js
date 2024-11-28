ee37bcffbfdc3169baebe81e5b4e7a912cb0d186
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-28 01:43:36 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
-
+////
 let graph1 = [
     [0, 1, 0],
     [1, 0, 1],
@@ -14,7 +14,9 @@ let graph2 = [
     [1, 0, 1],
     [0, 1, 0]
 ];
+////
 
+////
 let fullyConnected1 =[
     [0, 1, 1, 1],
     [1, 0, 1, 1],
@@ -28,7 +30,24 @@ let fullyConnected2 =[
     [1, 1, 0, 1],
     [1, 1, 1, 0]
 ];
+////
+
+////
+let notSame1 = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+];
+
+let notSame2 =[
+    [0, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0]
+];
+//
     
 
 assert(are_isomorphic(graph1, graph2) === true);
 assert(are_isomorphic(fullyConnected1, fullyConnected2) === true);
+assert(are_isomorphic(notSame1, notSame2) === false);
